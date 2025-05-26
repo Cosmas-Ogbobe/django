39b56df5f8c3b951ec86e020ff86cbffe5af6791
ogbobe <cosmas.akanna@gmail.com>
2025-05-26 11:26:09 +0100

    initial commit

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+def homePageView(request):
+    return HttpResponse("Welcome to the Home Page!")
 
 # Create your views here.
